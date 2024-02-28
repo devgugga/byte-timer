4aac39c62ba8c871456a1824425b29f1073094c4
Gustavo Gomes <120190910+devgugga@users.noreply.github.com>
2024-02-28 12:58:52 -0300

    :lipstick: Criando Home
    Nest commit foi criado e estilizado toda a pagina home.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,60 @@
+import { Play } from "phosphor-react";
+
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  PlayButton,
+  Separator,
+  TaskInput,
+} from "./styles.ts";
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Teste" />
+            <option value="Teste" />
+            <option value="Teste" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <PlayButton type="submit">
+          <Play size={24} />
+          Começar
+        </PlayButton>
+      </form>
+    </HomeContainer>
+  );
 }
